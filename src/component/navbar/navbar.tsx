a0ea88a019f99f777dae989365fd2cf14cffff01
Ansh Sharma <anshattrii12@gmail.com>
2025-04-08 18:04:10 +0530

    build upload

--- src/component/navbar/navbar.tsx
+++ src/component/navbar/navbar.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import * as PiIcons from "react-icons/pi";
+import {PiDiamondsFourFill}from "react-icons/pi";
 import { FaUserCircle } from "react-icons/fa";
 import { MdOutlineDarkMode } from "react-icons/md";
 const Navbar: React.FC = () => {
   return (
     <div className="flex w-full mt-3 justify-between gap-4 p-4">
           <div className="flex-1 ml-4 cursor-pointer">
-          <PiIcons.PiDiamondsFourFill size={26} className="text-[#6362E7]" />
+          <PiDiamondsFourFill size={26} className="text-[#6362E7]" />
           </div>
           <div className="flex gap-5 cursor-pointer mr-4">
             <FaUserCircle size={26}/>
