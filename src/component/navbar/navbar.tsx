2b13601e97b1ee14cd9f30e8cdf6ca9c302898e9
Ansh Sharma <anshattrii12@gmail.com>
2025-04-09 14:11:11 +0530

    build upload

--- src/component/navbar/navbar.tsx
+++ src/component/navbar/navbar.tsx
@@ -3,14 +3,18 @@ import { PiDiamondsFourFill } from "react-icons/pi";
 import { FaUserCircle } from "react-icons/fa";
 import { MdOutlineDarkMode } from "react-icons/md";
 const Navbar: React.FC = () => {
+  const DiamondIcon = PiDiamondsFourFill as unknown as React.FC<{ size?: number; className?: string }>;
+  const UserIcon = FaUserCircle as unknown as React.FC<{ size?: number; className?: string }>;
+  const DarkModeIcon = MdOutlineDarkMode as unknown as React.FC<{ size?: number; className?: string }>;
+
   return (
     <div className="flex w-full mt-3 justify-between gap-4 p-4">
           <div className="flex-1 ml-4 cursor-pointer">
-            <PiDiamondsFourFill size={26} className="text-[#6362E7]"/>
+            <DiamondIcon size={26} className="text-[#6362E7]"/>
           </div>
           <div className="flex gap-5 cursor-pointer mr-4">
-            <FaUserCircle size={26}/>
-            <MdOutlineDarkMode size={26}/>
+            <UserIcon size={26}/>
+            <DarkModeIcon size={26}/>
           </div>
       </div>
   );
